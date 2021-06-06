8d94d665e55d70a3c1b941ead6417bd31e105dfb
David Sn <divad.nnamtdeis@gmail.com>
2021-06-07 00:22:40 +0200

    Remove iblocklist.com update script and replace with blocklist.py
    Signed-off-by: David Sn <divad.nnamtdeis@gmail.com>

--- update-list.sh
+++ update-list.sh
@@ -1,15 +1,3 @@
 #!/bin/sh
-# Based on https://gist.github.com/johntyree/3331662
 
-USER_AGENT="Mozilla/5.0 (X11; Linux x86_64; rv:87.0) Gecko/20100101 Firefox/87.0"
-
-# Download lists, unpack and filter, write to gzipped file 
-curl -A "$USER_AGENT" -s https://www.iblocklist.com/lists.php \
-    | sed -n "s/.*value='\(http:.*\)'.*/\1/p" \
-    | sed "s/\&amp;/\&/g" \
-    | sed "s/http/\"http/g" \
-    | sed "s/gz/gz\"/g" \
-    | xargs curl -s -L \
-    | gunzip \
-    | egrep -v '^#' \
-    | gzip - > $OUTPUT_DIR/blocklist.p2p.gz
+blocklist.py && gzip "${OUTPUT_DIR}/blocklist.p2p"
